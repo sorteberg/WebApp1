1b0d5156144f92bd88372069833228148c0daa00
mads nyborg <nyborg.mads@gmail.com>
2013-11-06 16:22:13 +0100

    la på unittester

---
+++ MvcApplication.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MvcApplication.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Index_WhenIEatBanana_WillNotFail()
+        {
+            Assert.Fail("DENNE TESTEN ER GAMMEL OG DÅRLIG");
+        }
+
+        [Test]
+        public void Index_WhenStuffIsGoingOn_WillReturnStuff()
+        {
+            Assert.IsTrue(31 > 1);
+        }
+    }
+}
